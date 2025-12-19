c510e59463d8932fb1ec44ed7e6edf8173a4ef85
Mickael Guevel <127212295+Ultrax92@users.noreply.github.com>
2025-12-19 17:24:31 +0100

    moove style 404 in .css

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div style="margin-top: 50px;">
+    <div class="bloc-error">
         <h1>Page non trouvée.</h1>
         <a href="{{ route('home') }}" class="link-blue">Retour à l'accueil</a>
     </div>
